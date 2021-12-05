be69e8c708d761d37e07cbecfa48127d9f354d28
ram10cy <43674809+ram10cy@users.noreply.github.com>
2021-12-05 18:34:25 +0200

    Version 1.4 - Created google api helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,10 +1,69 @@
 <?php
 
 function getPostApiCoordinates($address){
+
     // to get locations of adresss
+
     $postCodesApiUrl = 'http://api.postcodes.io/postcodes/'.$address;
-    //read json file from url in php
+ // validate if address is valid
+    $validateAddress= $postCodesApiUrl.'/validate';
+    $readJSONFile = file_get_contents($validateAddress);
+    $array = json_decode($readJSONFile);
+    if($array->result==false)
+    {
+        $array->message='Address not found';
+        return $array;
+    }
+
+
+    //return address coordinates
     $readJSONFile = file_get_contents($postCodesApiUrl);
     $array = json_decode($readJSONFile);
     return $array->result;
+
+
+}
+
+function verifyAddress($userAddress,$contactAddress){
+    if(
+        isset($userAddress->latitude) and
+        isset($userAddress->longitude) and
+        isset($contactAddress->latitude) and
+        isset($contactAddress->longitude)
+    )
+       return True;
+    else
+      return False;
+
+
+}
+
+function getGoogleApiDurations($data){
+
+
+
+
+    $urlArrival='https://api.distancematrix.ai/maps/api/distancematrix/json?origins='.$data['user_latitude'].','.$data['user_longitude'].'&destinations='.$data['contact_latitude'].','.$data['contact_longitude'].'&arrival_time='.$data['meeting_time'].'&mode=driving&key=2e826TqOA3JE8rI3wmrbL3A31RmWd';
+    $urlDeparture='https://api.distancematrix.ai/maps/api/distancematrix/json?origins='.$data['contact_latitude'].','.$data['contact_longitude'].'&destinations='.$data['user_latitude'].','.$data['user_longitude'].'&departure_time='.$data['meeting_finish_time'].'&mode=driving&key=2e826TqOA3JE8rI3wmrbL3A31RmWd';
+
+    $readJSONFileArrival = file_get_contents($urlArrival);
+    $readJSONFileDeparture = file_get_contents($urlDeparture);
+
+    $arrivalDuration = json_decode($readJSONFileArrival)->rows[0]->elements[0]->duration;
+    $departureDuration = json_decode($readJSONFileDeparture)->rows[0]->elements[0]->duration;
+
+    $meetingTime=date('Y-m-d H:i',$data['meeting_time']);
+    $leaveOfficeTime=date('Y-m-d H:i',($data['meeting_time']-$arrivalDuration->value));
+    $returnToOfficeTime=date('Y-m-d H:i',($data['meeting_finish_time']+$departureDuration->value));
+
+    $array=[
+         'meetingTime'=>$meetingTime,
+        'leaveOfficeTime'=> $leaveOfficeTime,
+        'returnToOffice'=>$returnToOfficeTime
+    ];
+
+
+    return $array;
+
+
 }
--- app/Respositories/AppointmentRespository.php
+++ app/Respositories/AppointmentRespository.php
@@ -11,13 +11,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AppointmentRespository
 {
-    public function store($appointmentData){
+    public function create($appointmentData){
 
-        //Validate data
+        $user=auth()->user();
+       //Validate data
         $validator = Validator::make($appointmentData, [
             'appointmentAddress' => 'required|string',
-            'appointmentDate' => 'required|required|date_format:Y-m-d H:i',
-            'contactId'=>'required|number'
+
+       //     'appointmentDate' => 'required|required|date_format:Y-m-d H:i',
+            'contactId'=>'required|numeric',
+            'mode'=>'required',
         ]);
 
         //Send failed response if request is not valid
@@ -25,7 +28,44 @@ class AppointmentRespository
             return response()->json(['error' => $validator->messages()], 200);
         }
 
-        $user=auth()->user();
+
+        $userCoordinates=getPostApiCoordinates($user->address);
+        $contactCoordinates=getPostApiCoordinates($appointmentData['appointmentAddress']);
+
+        //check if address is true
+        if(!verifyAddress($userCoordinates,$contactCoordinates)==false)
+            return response()->json([
+                'success' => false,
+                'error' => 'Address Error',
+            ], Response::HTTP_OK);
+
+        //prepare data to send googleApiHelper
+        $apiData= [
+            'user_latitude'=>$userCoordinates->latitude,'user_longitude'=>$userCoordinates->longitude,
+            'contact_latitude'=>$contactCoordinates->latitude,'contact_longitude'=>$contactCoordinates->longitude,
+            'meeting_time'=>strtotime("12:00pm December 06 2021"),
+            'meeting_finish_time'=>strtotime("12:00pm December 06 2021")+3600,
+            'mode'=>$appointmentData['mode']
+        ];
+
+        $durations=  getGoogleApiDurations($apiData);
+
+
+
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Contact created successfully',
+            'locations'=>$apiData,
+            'test'=>$apiData['user_latitude'],
+
+            'addressVerified'=>verifyAddress($userCoordinates,$contactCoordinates),
+            'durations'=>$durations,
+            'userCoordinate' => $userCoordinates,
+            'contactCoordinate'=>$contactCoordinates
+        ], Response::HTTP_OK);
+
+
 
         //Request is valid, create new user
         $appointment = AppointmentModel::create([
